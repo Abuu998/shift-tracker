6f7e65274432f72c9dc0d52c7154f1a85d577ecb
Abubakar <abubakarndu@gmail.com>
2024-01-23 16:57:38 +0300

    feat: added a loader

--- components/DisplayDateTime.jsx
+++ components/DisplayDateTime.jsx
@@ -1,10 +1,21 @@
 "use client"
 
-import { Suspense } from "react"
+import { Suspense, useEffect, useState } from "react"
+import Loader from "./Loader"
+// import { HiArrowPath } from "react-icons/hi2";
 
 function DisplayDateTime({ children }) {
+    const [mounted, setMounted] = useState(false)
+
+    if(!mounted) return <Loader />
+
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
     return (
-        <Suspense fallback={<p>Loading time...</p>}>
+        <Suspense>
             { children }
         </Suspense>
     )
---
+++ components/Loader.jsx
@@ -0,0 +1,9 @@
+
+
+import { TbLoader } from "react-icons/tb";
+
+function Loader({ className="text-xl" }) {
+    return <TbLoader className={`animate-spin ${className}`} />
+}
+
+export default Loader
