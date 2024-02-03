160ec47a5ce508360d65f6227a5742cdd864b118
Abubakar <abubakarndu@gmail.com>
2024-02-03 21:18:51 +0300

    refactor: removed redudant deps

--- middleware.js
+++ middleware.js
@@ -1,17 +1,5 @@
-import { NextResponse } from "next/server";
-import NextCors from 'nextjs-cors';
-// import corsMiddleware from "./middleware/cors";
 
 export function middleware(req) {
-    // retrieve the current response
-    // const res = NextResponse.next()
-
-    // await NextCors(req, res, {
-    //     // Options
-    //     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    //     origin: 'http://localhost:5173',
-    //     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    // });
 
     if(req.method === 'OPTIONS') {
         return new Response({}, {
